4ebbe1dc4b730eb611ece3dcaa5d08f2e356f31d
Volative PC <volative.work@gmail.com>
2021-04-02 22:19:38 +0700

    controller router maneh

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UserController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function getUser()
+    {
+        return response()->json([
+            'message'=>'Mantab'
+        ]);
+
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Auth\Authenticatable;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Lumen\Auth\Authorizable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class User extends Model
 {
-    use Authenticatable, Authorizable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
